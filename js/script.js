3ba00c7448db27952587a7d9761d5b68612f0981
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-10-14 18:32:02 -0400

    Added jQuery and set up variables.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
+// Variables
 // Add variables that store DOM elements you will need to reference and/or manipulate
+const $pageHeader = $('.page-header');
+const $studentList = $('.student-list');
+const $studentItem = $('.student.item');
 
 
-
-// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Create a function to hide all of the items in the list except for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+function showSinglePage(qty, group) {
 
+}
 
 
 
@@ -20,9 +25,3 @@ FSJS project 2 - List Filter and Pagination
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
